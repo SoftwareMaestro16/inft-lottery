4efef92975bf19b75e21e82c7a072827bb290c06
Software Maestro <softwaremaestro16@gmail.com>
2024-10-11 21:36:21 +0300

    mainnet data

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 import {Address} from "@ton/core";
 
-export const USDT = Address.parse('kQDHlLcQ-OXz-G18tw8vAQgfzF5BOulntDoxZfoekDEBbyee');
+export const USDT = Address.parse('EQAEfUNvB01k3khyyMJeQu6Y609TOPtm_-Mn0-12NJb4SXwR');

762f29433a34ccdb7f6383507e929598c67cf2c5
Software Maestro <softwaremaestro16@gmail.com>
2024-10-11 21:53:34 +0300

    fees tx fix

--- src/SendTx.tsx
+++ src/SendTx.tsx
@@ -91,7 +91,7 @@ export const SendTx = ({ selectedAmount }: SendTxProps) => {
                 messages: [
                     {
                         address: jwAddress.toString(),
-                        amount: "112000000",
+                        amount: "114000000",
                         payload
                     }
                 ]
